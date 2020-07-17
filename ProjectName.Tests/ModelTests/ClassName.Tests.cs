a7bb5ddce32ddd3a0a4119f4942db5e81deaa748
Taylor Phillips <62128243+tayphill13@users.noreply.github.com>
2020-07-17 08:06:19 -0700

    Initial commit

---
+++ ProjectName.Tests/ModelTests/ClassName.Tests.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectName.Models;
+
+namespace ProjectName.Tests
+{
+  [TestClass]
+  public class ClassNameTests
+  {
+
+    [TestMethod]
+    public void NameOfMethod_DescriptionOfTest_ExpectedResult()
+    {
+      //Arrange
+      //Act
+      //Assert.AreEqual(ExpectedResult, Code_To_Test);
+    }
+  }
+}
---
+++ ProjectName/Program.cs
@@ -0,0 +1,15 @@
+using ProjectName.Models;
+using System;
+using System.Collections.Generic;
+
+
+namespace ProjectName
+{
+  public class Program
+  {
+    public static void Main()
+    {
+      Console.WriteLine("Hello World");
+    }
+  }
+}
