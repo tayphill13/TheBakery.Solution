836488b49fc3d07e73990ec70230ec381eb3f09d
Taylor Phillips <taylorphillips133@gmail.com>
2020-07-17 13:16:12 -0700

    add if else statement to the Program.cs to ask customer whether they're shopping today

--- TheBakery/Program.cs
+++ TheBakery/Program.cs
@@ -12,8 +12,18 @@ namespace TheBakery
       Console.WriteLine("Welcome to Pierre's Bakery! Our baked goods are great and our specials will keep you coming back for more");
       Console.WriteLine("------------------------------------------------------------------------");
       Console.WriteLine("Bread is $5 per loaf and if you buy 2, you can get a 3rd loaf for free!");
+      Console.WriteLine("Pastries are $2 and you can get 3 for $5  -----  Recommended by kids!");
       Console.WriteLine("------------------------------------------------------------------------");
-      Console.WriteLine("How many loaves of bread would you like today?");
-      int howManyLoaves = Console.ReadLine();
+      Console.WriteLine("Would you like some bread or a pastry?");
+      string yesOrNo = Console.ReadLine();
+      if (yesOrNo == "yes")
+      {
+        Console.WriteLine("How many loaves of bread would you like today?");
+      }
+      else if (yesOrNo == "no")
+      {
+        Console.WriteLine("Not today?  Ok, well thanks for coming in, please visit again soon.");
+      }
+    }
   }
 }
