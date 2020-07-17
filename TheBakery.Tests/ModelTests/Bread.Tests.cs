c9e7fdbe2c49ab3192218aa8b08afb8f7aab684e
Taylor Phillips <taylorphillips133@gmail.com>
2020-07-17 11:02:19 -0700

    add a few notes to self in the test

--- TheBakery.Tests/ModelTests/Bread.Tests.cs
+++ TheBakery.Tests/ModelTests/Bread.Tests.cs
@@ -8,11 +8,15 @@ namespace TheBakery.Tests
   {
 
     [TestMethod]
-    public void BreadConstructor_CreatesInstanceOfBread_True()
-    {
-      Bread testBread = new Bread(1);
-
+    public void BreadConstructor_CreatesInstanceOfBread_True()   // I'm not 100% sure if this is a valid test of the constructor, but I have 
+    {                                                            // but I have to move on.
+      Bread testBread = new Bread(1);                            // Passing in 1 loaf
       Assert.AreEqual(typeof(Bread), testBread.GetType());
+    }
+    [TestMethod]
+    public void MethodToTest_WhatImTestingFor_ExpectedOutput()
+    {
+      Bread testBread = new Bread();
     }
   }
 }
