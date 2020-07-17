465c91e033046eac52d279b3fa8f9b51766846ff
Taylor Phillips <taylorphillips133@gmail.com>
2020-07-17 16:21:08 -0700

    add ToLower.() so that users yes or no is not case sensitive

--- TheBakery/Program.cs
+++ TheBakery/Program.cs
@@ -7,15 +7,15 @@ namespace TheBakery
 {
   public class Program
   {
-    public static void Main(string[] args)
+    public static void Main()
     {
       Console.WriteLine("Welcome to Pierre's Bakery! Our baked goods are great and our specials will keep you coming back for more");
       Console.WriteLine("------------------------------------------------------------------------");
       Console.WriteLine("Bread is $5 per loaf and if you buy 2, you can get a 3rd loaf for free!");
-      Console.WriteLine("Pastries are $2 and you can get 3 for $5  -----  Recommended by kids!");
+      Console.WriteLine("For pastries we have Cinnamon Rolls, they're $2, or you can get 3 for $5  -----  Recommended by kids!");
       Console.WriteLine("------------------------------------------------------------------------");
       Console.WriteLine("Would you like some bread or a pastry?");
-      string yesOrNo = Console.ReadLine();
+      string yesOrNo = Console.ReadLine().ToLower();
       if (yesOrNo == "yes")
       {
         Console.Write("How many loaves of bread would you like today:  ");
