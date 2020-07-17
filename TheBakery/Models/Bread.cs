7b04c983653cf6b61594c6d579c837da6a7e6c07
Taylor Phillips <taylorphillips133@gmail.com>
2020-07-17 15:55:22 -0700

    show a passing test for when modulo is 1, special pricing with odd purchase

--- TheBakery/Models/Bread.cs
+++ TheBakery/Models/Bread.cs
@@ -1,3 +1,4 @@
+using System;
 
 namespace TheBakery.Models
 {
@@ -15,13 +16,20 @@ namespace TheBakery.Models
     {
       int breadTotalCost = 0; // Start totalCost at 0 
 
-      if(HowManyLoaves % 3 == 0)
+      if (HowManyLoaves % 3 == 0)
       {
         breadTotalCost = ((HowManyLoaves * 5) - ((HowManyLoaves/3) * CostPerLoaf));
       }
+      else if (HowManyLoaves < 3)
+      {
+        breadTotalCost = HowManyLoaves * CostPerLoaf;
+      }
       else
       {
-      breadTotalCost = (HowManyLoaves * 5);
+        if (HowManyLoaves % 3 == 1)
+        {
+          breadTotalCost = (CostPerLoaf * (HowManyLoaves - 1)) - ((HowManyLoaves/3) * CostPerLoaf) + CostPerLoaf;
+        }
       }
       return breadTotalCost;  // return totalCost after calculations
     }
